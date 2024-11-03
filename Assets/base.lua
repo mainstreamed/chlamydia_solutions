6e7f87df0540cce1feff3955ef4ddb58b0c191d0
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2024-11-03 16:48:30 +1000

    Create base.lua

---
+++ Assets/base.lua
@@ -0,0 +1,27 @@
+local gameName, sversion = ...;
+
+gameName = gameName or 'testing';
+sversion = sversion or 'developer';
+
+local repository        = 'https://raw.githubusercontent.com/mainstreamed/chlamydia_solutions/refs/heads/main/Assets/';
+
+local loadAsset         = function(connector)
+      local func, _err = loadstring(game:HttpGet(`{repository}{connector}`));
+      assert(func, `Failed to load Asset {connector}: {_err}`)
+      return func();
+end;
+
+local uiLibrary         = loadAsset('uiLibrary.lua');
+local themeManager      = loadAsset('themeManager.lua');
+local saveManager       = loadAsset('saveManager.lua');
+
+local window = uiLibrary:CreateWindow({
+      Title             = `Chlamydia Solutions {gameName} | version: {sversion}`;
+      Center            = true;
+      AutoShow          = true;
+      TabPadding        = 8;
+      MenuFadeTime      = 0.2;
+});
+
+
+return {window, themeManager, saveManager};
